29f29f4542a9036369929ca6aa94fa6e6cc71d1d
sahil jaiswal <sahil1905050310026@gmail.com>
2024-05-23 20:57:15 +0530

    code

---
+++ javascript/datatype.js
@@ -0,0 +1,17 @@
+//Datatype is responsible to define the type of data we will store in variable
+//Variable is a memory location where we are storing some information
+// Datatype can be devided into two types primitive and non primitive
+
+//Prmitive datatype-
+1- String ="Computer is a electronic device"
+2- Numbers =1,2,3,4,5.6,7.8
+3- Boolean = true and false
+4- null
+5- undefined
+6- BigInt = 131314124214
+7- Symbol = ""
+
+//Non Primitive Datatype
+1- Object
+2- Array
+3- Date
