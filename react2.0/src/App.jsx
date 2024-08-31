f6b2726dedc9820b235e07b88fc4389bd9c2124c
sahil jaiswal <sahil1905050310026@gmail.com>
2024-08-31 20:57:14 +0530

    code

--- react2.0/src/App.jsx
+++ react2.0/src/App.jsx
@@ -1,34 +1,13 @@
 import React from "react";
-import Navbar from "./component/Navbar";
-import Card from "./component/Card";
-import Table from "./component/Table";
+import Index from "./component/Index";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
-  const data = [
-    {
-      url: "https://static.displate.com/280x392/displate/2021-01-28/61f2a61d84a7c385afb8a4dd139810d3_9180d9b5b158d5b3dd86c841b1f7a500.jpg",
-      name: "Guardians of Galaxy",
-      desc: "Computer is a electronic machine",
-    },
-
-    {
-      url: "https://townsquare.media/site/442/files/2017/10/thor_ragnarok_ver2_xlg1.jpg",
-      name: "Thor Ragnorok",
-      desc: "Computer is a electronic machine",
-    },
-
-    {
-      url: "https://i.pinimg.com/736x/48/54/99/4854994674c2da4bc8ca3878c62a1546.jpg",
-      name: "Spider Man",
-      desc: "Computer is a electronic machine",
-    },
-  ];
-
   return (
-    <>
-      <Navbar />
-      <Card d={data} />
-      <Table />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- react2.0/src/component/Card.jsx
+++ react2.0/src/component/Card.jsx
@@ -1,47 +1,26 @@
 import React from "react";
 
-export default function Card({ d }) {
+export default function Card(props) {
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ marginTop: "50px" }}>
         <div class="row">
-          <div class="col">
-            <div class="card" style={{ width: "18rem" }}>
-              <img src={d[0].url} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">{d[0].name}</h5>
-                <p class="card-text">{d[0].desc}</p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
+          {props.d.map((v) => {
+            return (
+              <div class="col">
+                <div class="card" style={{ width: "18rem" }}>
+                  <img src={v.url} class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">{v.name}</h5>
+                    <p class="card-text">{v.desc}</p>
+                    <a href="#" class="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col">
-            {" "}
-            <div class="card" style={{ width: "18rem" }}>
-              <img src={d[1].url} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">{d[1].name}</h5>
-                <p class="card-text">{d[1].desc}</p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card" style={{ width: "18rem" }}>
-              <img src={d[2].url} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">{d[2].name}</h5>
-                <p class="card-text">{d[2].desc}</p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </>
